f7d9d511515defdaadca741ab940ae1f77aacee8
郑军 <zhengjun1987@outlook.com>
2018-04-09 23:44:23 +0800

    <The C Programming Language> 2nd Edition chapter01 Introduction 2018-04-09 23:44:11

---
+++ chapter01/another.c
@@ -0,0 +1,14 @@
+#include <printf.h>
+
+/**
+ *  Author:Zheng Jun
+ *  Date:2018/4/9 23:35
+ *  E-mail:zhengjun1987@outlook.com
+ *  Desc:
+ */
+
+main(){
+    printf("hello,");
+    printf("world");
+    printf("\n");
+}
---
+++ chapter01/first.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int fst() {
+    printf("Hello, World!\n");
+    return 0;
+}
